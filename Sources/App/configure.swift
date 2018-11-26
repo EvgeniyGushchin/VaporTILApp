1c3b8680f17ca0a01a589adabafa87745917ccce
Evgeniy Gushchin <evgeniy.gushchin@aldigit.com>
2018-11-26 15:28:28 +0600

    configuring heroku db

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -19,17 +19,22 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    let hostname = Environment.get("DATABASE_HOSTNAME")
-        ?? "localhost"
-    let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-    let password = Environment.get("DATABASE_PASSWORD")
-        ?? "password"
-    let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: hostname,
-        username: username,
-        database: databaseName,
-        password: password)
+    let databaseConfig: PostgreSQLDatabaseConfig
+    if let url = Environment.get("DATABASE_URL") {
+        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+    } else {
+        let hostname = Environment.get("DATABASE_HOSTNAME")
+            ?? "localhost"
+        let username = Environment.get("DATABASE_USER") ?? "vapor"
+        let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        let password = Environment.get("DATABASE_PASSWORD")
+            ?? "password"
+        databaseConfig = PostgreSQLDatabaseConfig(
+            hostname: hostname,
+            username: username,
+            database: databaseName,
+            password: password)
+    }
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
     services.register(databases)
