9b11b21b455ea1b56cdec8ca745afa1aa6dc2793
Evgeniy Gushchin <evgeniy.gushchin@aldigit.com>
2018-12-14 15:48:33 +0600

    Add Tests

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -20,17 +20,29 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     let databaseConfig: PostgreSQLDatabaseConfig
+    
+    let databaseName: String
+    let databasePort: Int
+    // 1
+    if (env == .testing) {
+        databaseName = "vapor-test"
+        databasePort = 5433
+    } else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        databasePort = 5432
+    }
+    
     if let url = Environment.get("DATABASE_URL") {
         databaseConfig = PostgreSQLDatabaseConfig(url: url)!
     } else {
         let hostname = Environment.get("DATABASE_HOSTNAME")
             ?? "localhost"
         let username = Environment.get("DATABASE_USER") ?? "vapor"
-        let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
         let password = Environment.get("DATABASE_PASSWORD")
             ?? "password"
         databaseConfig = PostgreSQLDatabaseConfig(
             hostname: hostname,
+            port:     databasePort,
             username: username,
             database: databaseName,
             password: password)
